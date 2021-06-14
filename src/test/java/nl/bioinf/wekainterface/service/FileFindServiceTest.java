6413ba8fd8a9dfdabbf6abe98acf07f77325df83
bengels <b.engels@st.hanze.nl>
2021-06-14 17:57:47 +0200

    First draft FilefindserviceTest

--- src/test/java/nl/bioinf/wekainterface/service/FileFindServiceTest.java
+++ src/test/java/nl/bioinf/wekainterface/service/FileFindServiceTest.java
@@ -11,13 +11,16 @@ class FileFindServiceTest {
     @Test
     void findFile() {
         String filePath = null;
-        File direcotryPath = new File("/Users/bengels/§Desktop/temp");
+        File direcotryPath = new File("/Users/bengels/Desktop/temp");
         File[] fileListIndirecotry = direcotryPath.listFiles();
+        String searchFile = "";
         if (fileListIndirecotry != null)
             for (File fileNamesInDirecotry : fileListIndirecotry) {
                 if (fileNamesInDirecotry.getName().contains("")) {
+                    searchFile = fileNamesInDirecotry.getName();
                     filePath = fileNamesInDirecotry.getAbsolutePath();
                 }
             }
+        assertEquals(filePath, direcotryPath + "/" + searchFile );
     }
 }
