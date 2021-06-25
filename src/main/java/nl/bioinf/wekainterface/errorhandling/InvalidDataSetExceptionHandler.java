41a733da5b7bb748dc85da2d5cb3503276a57c48
Jelle <j.d.becirspahic@st.hanze.nl>
2021-06-25 13:51:25 +0200

    Added loggers

--- src/main/java/nl/bioinf/wekainterface/errorhandling/InvalidDataSetExceptionHandler.java
+++ src/main/java/nl/bioinf/wekainterface/errorhandling/InvalidDataSetExceptionHandler.java
@@ -1,17 +1,21 @@
 package nl.bioinf.wekainterface.errorhandling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.text.SimpleDateFormat;
+
 @ControllerAdvice
 public class InvalidDataSetExceptionHandler {
-
+    Logger logger = LoggerFactory.getLogger(InvalidDataSetExceptionHandler.class);
     @ExceptionHandler(value = {InvalidDataSetException.class})
     public ResponseEntity<Object> handleLabelCounterException(InvalidDataSetException e){
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
-
+        logger.error(e.getMessage());
+        HttpStatus badRequest = HttpStatus.INTERNAL_SERVER_ERROR;
         InvalidDataSetException exception = new InvalidDataSetException(
                 e.getMessage(),
                 e,
